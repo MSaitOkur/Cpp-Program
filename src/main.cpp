a88130dfede673508bb200d9662f470322bc068c
Muhammed Sait Okur <muhammedsaitokur@gmail.com>
2022-12-06 05:32:53 +0200

    6_12_2022_1

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,3 @@
-#include <iostream>
-class A
-{
-public:
-    explicit A(double val)
-    {
-        std::cout << "explicit A(double), val = " << val << '\n';
-    }
-
-    A(int val)
-    {
-        std::cout << "A(int), val = " << val << '\n';
-    }
-};
-
 int main()
 {
-    A ax = 10.5; // geçerli
 }
